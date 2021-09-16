882516465ba00918f121392ed8320649b5c296ac
shnopy <shnopy319@gmail.com>
2021-09-16 21:53:43 +0100

    Added build, updated readme.md, updated package.json scripts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const args = process.argv.slice(2);
+var chromeDir;
 
 require("dotenv").config();
 
@@ -7,7 +9,10 @@ const {
   CCG_PASSWORD
 } = process.env;
 
-const args = process.argv.slice(2);
+if (!CCG_USERNAME || !CCG_PASSWORD) {
+  return console.log("Please include both a username and password");
+}
+
 
 async function login(page) {
   let loginForm = (await page.$x("//form[@id='login']"))[0];
@@ -18,6 +23,7 @@ async function login(page) {
 
   await usernameInput.type(CCG_USERNAME);
   await passwordInput.type(CCG_PASSWORD);
+
   await loginConfirm.click();
 }
 
@@ -26,6 +32,7 @@ async function findFeedback(obj) {
 
   for (let assignment in obj) {
     let currentAssignment = obj[assignment];
+
     let textContent = await (await currentAssignment.getProperty("textContent")).jsonValue();
 
     if (textContent.toLowerCase().includes("feedback")) {
@@ -36,11 +43,39 @@ async function findFeedback(obj) {
   return returnValue;
 }
 
+async function findTurnItInFeedback(obj, browser) {
+  let returnValue = 0;
+
+  for (let assignment in obj) {
+    let currentAssignment = obj[assignment];
+
+    let textContent = await (await currentAssignment.getProperty("textContent")).jsonValue();
+
+    if (textContent.toLowerCase().includes("feedback")) {
+      let page = await browser.newPage();
+
+      let parentElement = (await currentAssignment.$x(".."))[0];
+      let href = await (await parentElement.getProperty("href")).jsonValue();
+
+      await page.goto(href);
+
+      let feedback = (await page.$x("//div[@class='feedback']"))[0];
+      if (feedback) {
+        returnValue++;
+      }
+
+    }
+  }
+
+  return returnValue;
+}
+
 async function run(courseId) {
   console.log("Running");
 
-  let browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+  let browser = await puppeteer.launch({ headless: true, defaultViewport: null, executablePath: `${chromeDir}/chrome.exe` });
   let page = await browser.newPage();
+
   await page.goto("https://ccgonline.chichester.ac.uk/");
 
   await login(page);
@@ -49,40 +84,68 @@ async function run(courseId) {
   await page.goto(`https://ccgonline.chichester.ac.uk/course/view.php?id=${courseId}`);
 
   let openAllCoursesXPath = "//span[@id='toggles-all-opened' and @title='Open all topics']";
-  await page.waitForXPath(openAllCoursesXPath, { timeout: 5000 });
+  try {
+    await page.waitForXPath(openAllCoursesXPath, { timeout: 5000 });
+  } catch {
+    console.log("This course has no dropdown, program exiting");
+    await browser.close();
+    return;
+  }
+
 
   (await page.$x(openAllCoursesXPath))[0].click();
 
   let courseContent = (await page.$x("//section[@id='region-main']"))[0];
 
-  let notTurnItInFound = (await courseContent.$x("//span[@class='activity-mod-feedback']//a[contains(@href,'https://ccgonline.chichester.ac.uk/mod/assign/view.php')]"));
+  let notTurnItInFound = await courseContent.$x("//span[@class='activity-mod-feedback']//a[contains(@href,'https://ccgonline.chichester.ac.uk/mod/assign/view.php')]");
   let turnItInFound = await courseContent.$x("//a[contains(@href,'https://ccgonline.chichester.ac.uk/mod/assign/view.php')]//span[@class='instancename']");
 
   let allNotTurnItInFound = await findFeedback(notTurnItInFound);
-  let allTurnItInFound = await findFeedback(turnItInFound);
+  let allTurnItInFound = await findTurnItInFeedback(turnItInFound, browser);
 
   console.log(`${allNotTurnItInFound} pieces of non turn-it-in feedback found`);
   console.log(`${allTurnItInFound} pieces of turn-it-in feedback found`);
 
-  await browser.close();
+
+  setTimeout(async () => {
+    await browser.close();
+  }, 3000);
 }
 
 if (args.includes("-h") || args.includes("--help")) {
   console.log(`
     --help (-h): Brings up this help menu
     --course (-c): Selects a course id 
+    --chrome: Selects location of chrome.exe
   `);
 
   return;
-} else if (args.includes("-c") || args.includes("--course")) {
-  let providedCourseId = args[1];
-  console.log(`Setting course id to: ${providedCourseId}`);
+}
 
-  run(providedCourseId);
+if (args.includes("--chrome")) {
+  let argPos = args.indexOf("--chrome");
+  chromeDir = args[argPos + 1];
+}
 
-  return;
-} else if (args.length === 0) {
+if (args.includes("-c") || args.includes("--course")) {
+  let argPos = args.indexOf("-c") < 0 ? args.indexOf("--course") : args.indexOf("-c");
+  let providedCourseId = args[argPos + 1];
+
+  if (args[1]) {
+    console.log(`Setting course id to: ${providedCourseId}`);
+
+    run(providedCourseId);
+
+    return;
+  }
+}
+
+if (args.length === 0) {
   console.log("Please provide a valid course id");
 } else {
   console.log("Please provide a valid command line argument");
+}
+
+if (!args.includes("--chrome")) {
+  return console.log("Plesae provide a valid chrome location using '--chrome'");
 }
--- readme.md
+++ readme.md
@@ -2,10 +2,10 @@
 
 This project is a web scraper created using [Puppeteer](https://github.com/puppeteer/puppeteer) to gather the amount of feedback on specific course pages on [CCGOnline](https://ccgonline.chichester.ac.uk)
 
-## Dependencies
+## How to run
 
-NodeJs 10.18.1 or greater
+- When running the program you must use the `--chrome` option to specify your [Google Chrome](https://www.google.com/intl/en_uk/chrome/) install directory. Do not point this to the .exe, just to the folder
 
-## Usage
-`node . -h` or `node . --help` -> Brings up help information
-`node . -c` or `node . --course` -> Selects a course id
+- To run the program you must fill out the `.env.example` file and rename it to `.env`. This file should be placed in the same directory as the .exe file
+
+- When running first run the .exe file with the argument `-h` as this will bring up a help menu
